43b816922c8dfc165af7a7629c888f6e1400feb2
Rodrigo Carreira <rodrigocarreira33@gmail.com>
2024-10-14 19:28:16 +0100

    Animation tweaks and inputs

--- scripts/main.js
+++ scripts/main.js
@@ -5,35 +5,86 @@ import * as SGI_Example from "./example_scene.min.js";
 import { ANIMATION_NAMES, AnimationsMap, PlayAnimation } from "./animations.js";
 
 // Model parts
-let suporte, lampada_cilindrica, lampada_esferica;
+let suporte, lampada_cilindrica, lampada_esferica, abajurMesh;
 
 // Environment
-let mixer, scene, renderer, camera, controls, canvas;
+let mixer, scene, renderer, camera, controls, canvas, textureLoader;
 
 // Constants
-const cor_default = new THREE.Color("lightblue");
-const animate_btn = document.getElementById("animate-btn");
-const halfAnimate_btn = document.getElementById("half-animate-btn");
+const cor_default = new THREE.Color(0xffffff);
+const color_picker = document.getElementById("color-picker");
 
-animate_btn.addEventListener("click", () => {
+const abajur_btn = document.getElementById("abajur-btn");
+
+abajur_btn.addEventListener("change", (e) => {
   if (!mixer) {
     return;
   }
 
-  PlayAnimation(AnimationsMap.get(ANIMATION_NAMES.ABAJUR_L).animation);
-});
-
-halfAnimate_btn.addEventListener("click", () => {
-  if (!mixer) {
-    return;
-  }
-
-  let animation = AnimationsMap.get(ANIMATION_NAMES.ABAJUR_R);
-
+  const name = e.target.value > 0 ? ANIMATION_NAMES.ARM_TO_ABAJUR_F : ANIMATION_NAMES.ARM_TO_ABAJUR_B;
+  let animation = AnimationsMap.get(name);
   animation.state = PlayAnimation(animation.animation, animation.state);
 });
 
+const longArm_btn = document.getElementById("longArm-btn");
+
+longArm_btn.addEventListener("change", (e) => {
+  if (!mixer) {
+    return;
+  }
+
+  const name = e.target.value > 0 ? ANIMATION_NAMES.LONG_ARM_F : ANIMATION_NAMES.LONG_ARM_B;
+  let animation = AnimationsMap.get(name);
+  animation.state = PlayAnimation(animation.animation, animation.state);
+});
+
+const shortArm_btn = document.getElementById("shortArm-btn");
+
+shortArm_btn.addEventListener("change", (e) => {
+  if (!mixer) {
+    return;
+  }
+
+  const name = e.target.value > 0 ? ANIMATION_NAMES.SHORT_ARM_F : ANIMATION_NAMES.SHORT_ARM_B;
+  let animation = AnimationsMap.get(name);
+  animation.state = PlayAnimation(animation.animation, animation.state);
+});
+
+const support_btn = document.getElementById("support-btn");
+
+support_btn.addEventListener("change", (e) => {
+  if (!mixer) {
+    return;
+  }
+
+  const name = e.target.value > 0 ? ANIMATION_NAMES.SUPORT_L : ANIMATION_NAMES.SUPORT_R;
+  let animation = AnimationsMap.get(name);
+  animation.state = PlayAnimation(animation.animation, animation.state);
+});
+
+const armToAbajur_btn = document.getElementById("armToAbajur-btn");
+
+armToAbajur_btn.addEventListener("change", (e) => {
+  if (!mixer) {
+    return;
+  }
+
+  const name = e.target.value > 0 ? ANIMATION_NAMES.SUPORT_L : ANIMATION_NAMES.SUPORT_R;
+  let animation = AnimationsMap.get(name);
+  animation.state = PlayAnimation(animation.animation, animation.state);
+});
+
+color_picker.addEventListener("change", (event) => {
+  const texture = textureLoader.load("/images/texture.png");
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  // create material from color
+  const color = new THREE.Color(event.target.value);
+  material.color = color;
+  abajurMesh.material = material;
+});
+
 // Criar cena do threeJS e expor na consola
+textureLoader = new THREE.TextureLoader();
 scene = new THREE.Scene();
 window.cena = scene;
 
@@ -52,9 +103,10 @@ camera = new THREE.PerspectiveCamera(
   1000,
 );
 controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(-6, 2, 8);
-camera.lookAt(0, -1, 2.5);
-controls.target.set(0, -1, 2.5);
+camera.position.set(-10, 5, 8);
+camera.lookAt(10, 10, 10);
+console.log(camera)
+controls.update();
 
 // Carregar modelo, ajustar luzes, e preparar cena exemplo
 new GLTFLoader().load(
@@ -76,7 +128,7 @@ new GLTFLoader().load(
     });
 
     suporte = scene.getObjectByName("Support");
-    console.log(suporte);
+    abajurMesh = scene.getObjectByName("AbajurMesh");
 
     // Configurar das fontes luminosas do modelo
     const ponto_luminoso = scene.getObjectByName("Point");
